9c804106a7ea956f6ef401097ed6c6e50fdb8c74
Nematulloh <nematulloh111@gmail.com>
2023-07-26 18:31:47 +0500

    Task 2

--- Day1/Quadratic Equations/Quadratic Equations Task 2/Program.cs
+++ Day1/Quadratic Equations/Quadratic Equations Task 2/Program.cs
@@ -5,21 +5,35 @@ Console.WriteLine("Программа для решение квадратных
 
 Console.Write("Вводите а: ");
 
-int a = int.Parse(Console.ReadLine());
+double a = double.Parse(Console.ReadLine());
 
 Console.Write("Вводите b: ");
 
-int b = int.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
 
 Console.Write("Вводите c: ");
 
-int c = int.Parse(Console.ReadLine());
+double c = double.Parse(Console.ReadLine());
+
+double discriminant = b * b - 4 * a * c;
+
+Console.WriteLine($"Дискриминант: {discriminant}");
 
 
-int result = 0;
+if (discriminant > 0)
+{
+    double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+    double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+    Console.WriteLine($"x1 = {x1}");
+    Console.WriteLine($"x2 = {x2}");
+}
 
-int x = 0;
-
-result = (int)(a * Math.Pow(x, 2) + b * x + c);
-
-Console.WriteLine(result);
+else if (discriminant == 0)
+{
+    double x = -b / (2 * a);
+    Console.WriteLine($"Уравнение имеет один корень: x = {x}");
+}
+else
+{
+    Console.WriteLine("Уравнение не имеет действительных корней");
+}
