fc7b3406d3b0f1b9aa2867485269b7c4f7c4621f
Nematulloh <nematulloh111@gmail.com>
2023-07-26 18:10:08 +0500

    WareHous

--- Day1/Quadratic Equations/Quadratic Equations Task 2/Program.cs
+++ Day1/Quadratic Equations/Quadratic Equations Task 2/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello");
+﻿using System;
+Console.WriteLine("Hello");
+
+Console.WriteLine("Программа для решение квадратных уравнений ax^2+ bx + c = 0");
+
+Console.Write("Вводите а: ");
+
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Вводите b: ");
+
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Вводите c: ");
+
+int c = int.Parse(Console.ReadLine());
+
+
+int result = 0;
+
+int x = 0;
+
+result = (int)(a * Math.Pow(x, 2) + b * x + c);
+
+Console.WriteLine(result);
