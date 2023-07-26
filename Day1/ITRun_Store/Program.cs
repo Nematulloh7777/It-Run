bd0f4939f14e26e89e0e82597f386b936b5581fa
Nematulloh <nematulloh111@gmail.com>
2023-07-26 17:51:11 +0500

    WareHous

--- Day1/ITRun_Store/Program.cs
+++ Day1/ITRun_Store/Program.cs
@@ -1,39 +1,44 @@
-using System;
+
+
+
 using System.Collections.Generic;
 using System.Text;
-using System.Threading.Channels;
+
 
 
 string[] products = File.ReadAllLines("store.txt", Encoding.UTF8);
 while (true)
 {
+    Console.WriteLine("Hello!");
     Console.WriteLine(@"Выберите действие:
 a) Показать список продуктов
 b) Добавить новый продукт
 с) Продать продукт");
-    string UserInput = Console.ReadLine();
 
-    if (UserInput == "a")
+    string userInput = Console.ReadLine();
+    if (userInput == "a")
     {
+        Console.Clear();
         foreach (var product in products)
         {
             Console.WriteLine(product);
-
         }
     }
-    else if (UserInput == "b")
+    else if (userInput == "b")
     {
-        Console.Write("Вводите имя: ");
-        string newProductname = Console.ReadLine();
-        Console.Write("Вводите количество: ");
-        int NewProductQuantity = int.Parse(Console.ReadLine());
+        Console.Clear();
+
+        Console.Write("Вводите имя продукта: ");
+        string newProductName = Console.ReadLine();
+        Console.Write("Вводите количество продукта: ");
+        int newProductQuantity = int.Parse(Console.ReadLine());
         bool flag = true;
         for (int i = 0; i < products.Length; i++)
         {
-            if (products[i].StartsWith(newProductname))
+            if (products[i].StartsWith(newProductName))
             {
                 var temp = products[i].Split()[1];
-                products[i] = newProductname + " " + (int.Parse(temp) + NewProductQuantity);
+                products[i] = newProductName + " " + (int.Parse(temp) + newProductQuantity);
                 flag = false;
                 break;
             }
@@ -41,45 +46,46 @@ b) Добавить новый продукт
 
         if (flag)
         {
+
             string[] newProds = new string[products.Length + 1];
             for (int i = 0; i < products.Length; i++)
             {
                 newProds[i] = products[i];
             }
-            newProds[newProds.Length - 1] = newProductname + " " + NewProductQuantity;
+            newProds[newProds.Length - 1] = newProductName + " " + newProductQuantity;
             products = newProds;
         }
         File.WriteAllLines("store.txt", products);
-
     }
-    else if (UserInput == "c")
+    else if (userInput == "c")
     {
-        Console.Write("Вводите имя для продажи: ");
-        string Productname = Console.ReadLine();
+        Console.Clear();
+
+        Console.Write("Вводите имя продукта для продажи: ");
+        string productName = Console.ReadLine();
         Console.Write("Вводите количество: ");
-        int ProductQuantity = int.Parse(Console.ReadLine());
+        int productQuantity = int.Parse(Console.ReadLine());
         bool productExists = false;
         for (int i = 0; i < products.Length; i++)
         {
-            if (products[i].StartsWith(Productname))
+            if (products[i].StartsWith(productName))
             {
                 productExists = true;
                 var temp = products[i].Split()[1];
-                var check = int.Parse(temp) - ProductQuantity;
+                var check = int.Parse(temp) - productQuantity;
                 if (check < 0)
                 {
-                    Console.WriteLine("Недостаточно товаров в списке");
+                    Console.WriteLine("<Недостаточно товаров в списке>");
                 }
                 else
                 {
-                    products[i] = Productname + " " + check;
+                    products[i] = productName + " " + check;
                 }
             }
         }
         if (!productExists)
         {
             Console.WriteLine("\nПродукт не существует");
-            
         }
     }
 }
